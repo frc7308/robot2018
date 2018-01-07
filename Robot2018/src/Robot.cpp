12bafd6403c74098b238751ba25159dabb2eaca4
Isaac Wilcove <isaac@wilcove.com>
2018-01-07 00:10:44 -0800

    added first code

---
+++ Robot2018/src/Robot.cpp
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+#include <Joystick.h>
+#include <RobotDrive.h>
+
+#include <WPILib.h>
+
+class Robot : public frc::IterativeRobot {
+	RobotDrive drive; // robot drive system
+	Joystick stick; // only joystick
+public:
+	Robot() :
+		/* Initializes the variables as part of the constructor using an Initialization
+		 * list.
+		 */
+		drive(0,1), // these must be initialized in the same order
+		stick(1),	// as they are declared above.
+		autoLoopCounter(0) {
+			drive.SetExpiration(0.1);
+			/*
+			* Performs robot initialization
+			* (in this case sets the safety timer expiration for
+			* the myRobot object to .1 seconds, see the next step for an explanation of
+			* the motor safety timers).
+			*/
+		}
+
+	/*
+	 * This autonomous (along with the chooser code above) shows how to
+	 * select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * GetString line to get the auto name from the text box below the Gyro.
+	 *
+	 * You can add additional auto modes by adding additional comparisons to
+	 * the
+	 * if-else structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as
+	 * well.
+	 */
+	void AutonomousInit() override {
+		m_autoSelected = m_chooser.GetSelected();
+		// m_autoSelected = SmartDashboard::GetString(
+		// 		"Auto Selector", kAutoNameDefault);
+		std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void AutonomousPeriodic() {
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void TeleopInit() {}
+
+	void TeleopPeriodic() {}
+
+	void TestPeriodic() {}
+
+private:
+	frc::LiveWindow& m_lw = *LiveWindow::GetInstance();
+	frc::SendableChooser<std::string> m_chooser;
+	const std::string kAutoNameDefault = "Default";
+	const std::string kAutoNameCustom = "My Auto";
+	std::string m_autoSelected;
+};
+
+START_ROBOT_CLASS(Robot)
